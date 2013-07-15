ec43b528e04a113a58ee8b254a3a335055326ee0
Kyle Davis <kyledavis65@gmail.com>
2013-07-14 23:46:55 -0500

    Added Headshot Multiplier To AK47

--- terrortown/entities/weapons/weapon_ttt_ak47/shared.lua
+++ terrortown/entities/weapons/weapon_ttt_ak47/shared.lua
@@ -28,6 +28,7 @@ SWEP.Primary.ClipMax = 90
 SWEP.Primary.DefaultClip = 30
 SWEP.AutoSpawnable      = true
 SWEP.AmmoEnt = "item_ammo_smg1_ttt"
+SWEP.HeadshotMultiplier = 1.25
 
 SWEP.Pistol = false
 SWEP.Shotgun = false
